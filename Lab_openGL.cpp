59eb8ba5257074ddfe1a2f1da52e75551bebcea7
Vadim <48867558+AdequateV@users.noreply.github.com>
2023-03-04 21:02:06 +0500

    Add files via upload

---
+++ Lab_openGL.cpp
@@ -0,0 +1,176 @@
+// Lab_openGL.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "Header.h"
+
+GLuint VBO;
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+#define TUT3
+
+#pragma region TUTORIAL1
+#ifdef TUT1
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+
+    InitializeGlutCallbacks();
+
+    glClearColor(.15f, .78f, .37f, 0.0f);
+
+    glutMainLoop();
+
+    return 0;
+}
+#endif
+#pragma endregion
+
+
+#pragma region TUTORIAL2
+#ifdef TUT2
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_POINTS, 0, 1);
+
+    glDisableVertexAttribArray(0);
+
+    glutSwapBuffers();
+}
+
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer()
+{
+    Vector3f Vertices[1];
+    Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 02");
+
+    InitializeGlutCallbacks();
+
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
+
+    glutMainLoop();
+
+    return 0;
+}
+#endif
+#pragma endregion
+
+
+#pragma region TUTORIAL3
+#ifdef TUT3
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+
+    glutSwapBuffers();
+}
+
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer()
+{
+    Vector3f Vertices[3];
+    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
+    Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);
+    Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 03");
+
+    InitializeGlutCallbacks();
+
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
+
+    glutMainLoop();
+
+    return 0;
+}
+#endif
+#pragma endregion
+
