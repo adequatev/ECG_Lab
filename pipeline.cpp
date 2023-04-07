678b1df0a4d191e18e94bc367881cb4c8a29123f
Vadim <48867558+AdequateV@users.noreply.github.com>
2023-04-07 18:41:02 +0500

    Laboratory work 2

--- pipeline.cpp
+++ pipeline.cpp
@@ -55,7 +55,20 @@ void Pipeline::InitPerspectiveProj(Matrix4f& m) const
     m.m[2][0] = 0.0f;                   m.m[2][1] = 0.0f;            m.m[2][2] = (-zNear - zFar) / zRange; m.m[2][3] = 2.0f * zFar * zNear / zRange;
     m.m[3][0] = 0.0f;                   m.m[3][1] = 0.0f;            m.m[3][2] = 1.0f;                   m.m[3][3] = 0.0;
 }
-
+//const Matrix4f* Pipeline::GetTrans()
+//{
+//    Matrix4f ScaleTrans, RotateTrans, TranslationTrans, CameraTranslationTrans, CameraRotateTrans, PersProjTrans;
+//
+//    ScaleTrans.InitScaleTransform(m_scale.x, m_scale.y, m_scale.z);
+//    RotateTrans.InitRotateTransform(m_rotateInfo.x, m_rotateInfo.y, m_rotateInfo.z);
+//    TranslationTrans.InitTranslationTransform(m_worldPos.x, m_worldPos.y, m_worldPos.z);
+//    CameraTranslationTrans.InitTranslationTransform(-m_camera.Pos.x, -m_camera.Pos.y, -m_camera.Pos.z);
+//    CameraRotateTrans.InitCameraTransform(m_camera.Target, m_camera.Up);
+//    PersProjTrans.InitPersProjTransform(m_persProj.FOV, m_persProj.Width, m_persProj.Height, m_persProj.zNear, m_persProj.zFar);
+//
+//    m_transformation = PersProjTrans * CameraRotateTrans * CameraTranslationTrans * TranslationTrans * RotateTrans * ScaleTrans;
+//    return &m_transformation;
+//}
 
 const Matrix4f* Pipeline::GetTrans()
 {
